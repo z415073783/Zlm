c9599db36378bfe5279dae39e9b62fb578064d87
z415073783 <415073783@qq.com>
2016-06-14 20:16:45 +0800

    Initial Commit

---
+++ Zlm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zlm
+//
+//  Created by 曾亮敏 on 16/6/14.
+//  Copyright © 2016年 zlm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Zlm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zlm
+//
+//  Created by 曾亮敏 on 16/6/14.
+//  Copyright © 2016年 zlm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
